a6a8b02a8b29ce0b4ca98b1eaf9bb14ccca29368
Jian Wei Ng <s10208269@connect.np.edu.sg>
2023-02-03 00:49:11 +0800

    feat: Added company_id primary key and foreign key relationship between company data and student data, updated upload function

--- app.py
+++ app.py
@@ -17,6 +17,7 @@ class Student_Data(db.Model):
     Name = db.Column(db.String(255), nullable=False)
     Preference = db.Column(db.String(255), nullable=False)
     Status = db.Column(db.Enum('Unassigned', 'Pending confirmation', 'Confirmed', 'New Status'), nullable=False)
+    company_id = db.Column(db.Integer, db.ForeignKey('Company_Data.Company_ID'), nullable=True)
 
     def __repr__(self):
         return '<Student ID: %r>' % self.Student_ID
@@ -25,15 +26,17 @@ class Student_Data(db.Model):
 # Create company tables
 class Company_Data(db.Model):
     __tablename__ = 'Company_Data'
-    Company_Name = db.Column(db.String(255), primary_key=True)
+    Company_ID = db.Column(db.Integer, primary_key=True)
+    Company_Name = db.Column(db.String(255), nullable=False)
     Job_Role = db.Column(db.String(255), nullable=False)
     Company_Contact = db.Column(db.String(255), nullable=False)
     Email = db.Column(db.String(255), nullable=False)
     __table_args__ = (
         CheckConstraint('email LIKE "%@%.%"', name='valid_email'),
     )
+
     def __repr__(self):
-        return '<Company_Name: %r>' % self.Company_Name
+        return '<Company_ID: %r>' % self.Company_ID
 
 
 @app.route('/upload', methods=['POST'])
@@ -47,21 +50,37 @@ def upload():
         try:
             if student_data.content_type == 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet':
                 student_df = pd.read_excel(student_data)
-                if set(student_df.columns) == set(['Student_ID', 'Name', 'Preference', 'Status']):
+                if set(student_df.columns) == set(['Student_ID', 'Name', 'Preference', 'Status', 'Company_ID']):
                     for index, row in student_df.iterrows():
-                        student = Student_Data.query.filter_by(Student_ID=row['Student_ID']).first()
-                        if not student:
-                            student = Student_Data(Student_ID=row['Student_ID'], Name=row['Name'],
-                                                   Preference=row['Preference'], Status=row['Status'])
-                            db.session.add(student)
-                            db.session.commit()
+                        if 'Company_ID' in row and not pd.isna(row['Company_ID']):
+                            company = Company_Data.query.filter_by(Company_ID=row['Company_ID']).first()
+                            if not company:
+                                return f"<script> alert('The Company ID is not valid'); window.history.back(); </script>"
+                            else:
+                                student = Student_Data.query.filter_by(Student_ID=row['Student_ID']).first()
+                                if not student:
+                                    student = Student_Data(Student_ID=row['Student_ID'], Name=row['Name'],
+                                                           Preference=row['Preference'], Status=row['Status'],
+                                                           company_id=row['Company_ID'])
+                                    db.session.add(student)
+                                    db.session.commit()
+                                else:
+                                    continue
                         else:
-                            continue
+                            student = Student_Data.query.filter_by(Student_ID=row['Student_ID']).first()
+                            if not student:
+                                student = Student_Data(Student_ID=row['Student_ID'], Name=row['Name'],
+                                                       Preference=row['Preference'], Status=row['Status'],
+                                                       company_id=None)
+                                db.session.add(student)
+                                db.session.commit()
+                            else:
+                                continue
                     return redirect('/')
                 else:
-                    return 'The columns in the excel file does not match the columns in the table'
+                    return f"<script> alert('The columns in the excel file does not match the columns in the table'); window.history.back(); </script>"
             else:
-                return 'The file is not in excel format'
+                return f"<script> alert('The file is not in excel format'); window.history.back(); </script>"
         except IntegrityError:
             print("record already exists")
             db.session.rollback()
@@ -71,31 +90,44 @@ def upload():
         try:
             if company_data.content_type == 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet':
                 company_df = pd.read_excel(company_data)
-                if set(company_df.columns) == set(['Company_Name', 'Job_Role', 'Company_Contact', 'Email']):
+
+                if set(company_df.columns) == set(
+                        ['Company_Name', 'Job_Role', 'Company_Contact', 'Email']):
                     for index, row in company_df.iterrows():
-                        company = Company_Data.query.filter_by(Company_Name=row['Company_Name']).first()
-                        if not company:
-                            company = Company_Data(Company_Name=row['Company_Name'], Job_Role=row['Job_Role'],
-                                                   Company_Contact=row['Company_Contact'], Email=row['Email'])
-                            db.session.add(company)
-                            db.session.commit()
-                            return redirect('/')
-                    else:
-                        return 'The columns in the excel file does not match the columns in the table'
+                        company = Company_Data(Company_Name=row['Company_Name'],
+                                               Job_Role=row['Job_Role'], Company_Contact=row['Company_Contact'],
+                                               Email=row['Email'])
+                        db.session.add(company)
+                        db.session.commit()
+                    return redirect('/')
                 else:
-                    return 'The file is not in excel format'
+                    return f"<script> alert('The columns in the excel file does not match the columns in the table'); window.history.back(); </script>"
+            else:
+                return f"<script> alert('The file is not in excel format'); window.history.back(); </script>"
         except IntegrityError:
             print("record already exists")
             db.session.rollback()
             return redirect('/')
     else:
-        return 'Please upload either student or company data'
-
+        return f"<script> alert('Please upload either student or company data'); window.history.back(); </script>"
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return redirect("/upload_data", code=302)
+
+@app.route('/upload_data')
+def upload_data():
+    return render_template('upload_data.html')
+
+@app.route('/match_student')
+def match_student():
+    data = db.session.execute(db.select(Student_Data)).scalars()
+    print(data)
+    #tasks = Student.query
+    #students = Student.query.all()
+    return render_template('match_student.html', students=data)
+
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(port=5221, debug=True)
