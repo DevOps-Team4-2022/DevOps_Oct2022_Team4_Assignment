227665fe22fe51e36f0f058b0129ea99dd2f8495
vanweiting <your email>
2023-02-13 01:19:34 +0800

    improved test for navigation bar

--- test_navigationBar.py
+++ test_navigationBar.py
@@ -11,6 +11,11 @@ def test_uploadDataToMatchStudentPage():
     element = chrome_driver.find_element(By.XPATH, "/html/body/nav/div/ul/li[2]/a")
     element.click()
 
+    # check succesfully got to Match Students page
+    if not "Match Student" in chrome_driver.title:
+        raise Exception("Incorrect page name")
+    time.sleep(2)
+
 # # Upload Data page to Match Students page to Prepare Emails page
 # def test_matchStuedntsToPrepareEmailsPage():
 #     chrome_driver = webdriver.Chrome()
@@ -21,6 +26,11 @@ def test_uploadDataToMatchStudentPage():
 #     element = chrome_driver.find_element(By.XPATH, "...") # replace "..." with button XPATH
 #     element.click()
 
+#     # check succesfully got to Prepare Emails page
+#     if not "Prepare Emails" in chrome_driver.title:
+#         raise Exception("Incorrect page name")
+#     time.sleep(2)
+
 # # Upload Data page to Prepare Emails page to Settings page
 # def test_prepareEmailsToSettingsPage():
 #     chrome_driver = webdriver.Chrome()
@@ -30,4 +40,9 @@ def test_uploadDataToMatchStudentPage():
 #     element.click()
 #     element = chrome_driver.find_element(By.XPATH, "...") # replace "..." with button XPATH
 #     element.click()
+
+#     # check succesfully got to Settings page
+#     if not "Settings" in chrome_driver.title:
+#         raise Exception("Incorrect page name")
+#     time.sleep(2)
 
